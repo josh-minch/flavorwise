a831e11091c31372822b02d3eb3ebc391b993ce9
Josh Minch <josh.minch@gmail.com>
2020-10-17 18:58:26 -0700

    Add session storage for cur ingreds

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, session
 
-from matrix import get_similar_ingreds_np
+import matrix
+
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route("/")
 def index():
-    return render_template('main.html')
+    session.clear()
+    return render_template('index.html')
 
 
 @app.route("/search", methods=["POST"])
 def search():
-    ingred = request.form.get("search", 0, type=str)
-    ingreds = get_similar_ingreds_np(ingred)[:100]
-    result = ''
-    for ingred in ingreds:
-        result += '{}, '.format(ingred)
+    input_ingred = request.form.get("search", 0, type=str).strip()
+    cur_ingreds = update_session(input_ingred)
+    r_ingreds, recipes = matrix.search(cur_ingreds)
 
-    return jsonify(result=result)
+    result = ''
+    for ingred in r_ingreds:
+        result += '{} {}, '.format(ingred, r_ingreds[ingred])
+
+    return jsonify(cur_ingreds=cur_ingreds, r_ingreds=result, recipes=recipes)
+
+
+def update_session(input_ingred):
+    if 'cur_ingreds' not in session or session['cur_ingreds'] is None:
+        session['cur_ingreds'] = [input_ingred]
+    elif input_ingred in session['cur_ingreds']:
+        pass
+    else:
+        session['cur_ingreds'].append(input_ingred)
+        session.modified = True
+
+    return session['cur_ingreds']
