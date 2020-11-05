6a78105a8eb6704aefbfe7f481a19380a94a0be6
Josh Minch <josh.minch@gmail.com>
2020-11-04 23:03:54 -0800

    Add query string to force css and js refresh

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 ALL_INGREDS = [ingred.lower() for ingred in get_json('all_ingreds_filtered.json')]
-N_RECIPES = 142
-N_R_INGREDS = 60
+N_RECIPES = 99
+N_R_INGREDS = 120
 
 # TODO: Move session storage from flask session to .js SessionStorage
 #       Try render_template instead of js stuff? Just use js for fetch?
