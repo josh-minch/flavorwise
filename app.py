71b21798c91c9506b115df95e9110ea462712b6e
Josh Minch <josh.minch@gmail.com>
2020-11-05 12:15:26 -0800

    Add more debug statements

--- app.py
+++ app.py
@@ -101,8 +101,11 @@ def add_session_ingreds(new_ingreds):
 def remove_session_ingreds(ingreds_to_remove):
     for ingred in ingreds_to_remove:
         if ingred in session['cur_ingreds']:
+            print(session['cur_ingreds'])
+            print('removing ' + ingred)
             session['cur_ingreds'].remove(ingred)
             session.modified = True
+            print(session['cur_ingreds'])
     return session['cur_ingreds']
 
 
