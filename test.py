5dd647de04c228716cfd2d8951cd157f1637adf9
Josh Minch <josh.minch@gmail.com>
2020-11-05 17:36:13 -0800

    Add lemmatize tests, fix check_ingred tests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from scrape import check_ingred, create_ingred_filters, create_filter_prog, filter_naive
+from parse import (check_ingred, create_filter_prog, create_ingred_filters,
+                   filter_naive, lemmatize)
 
 
 class TestScrape(unittest.TestCase):
@@ -15,16 +16,19 @@ class TestScrape(unittest.TestCase):
 
     def test_check_ingred(self):
         self.assertEqual(check_ingred('apple', self.g_prog), 'apple')
-        self.assertEqual(check_ingred('apples', self.g_prog), 'apples')
-        self.assertEqual(check_ingred('Golden Delicious apples', self.g_prog), 'apples')
+        self.assertEqual(check_ingred('apples', self.g_prog), 'apple')
+        self.assertEqual(check_ingred(
+            'Golden Delicious apples', self.g_prog), 'apple')
 
-        self.assertEqual(check_ingred('apple sauce', self.s_prog), 'apple sauce')
+        self.assertEqual(check_ingred(
+            'apple sauce', self.s_prog), 'apple sauce')
         self.assertEqual(check_ingred('apple', self.s_prog), None)
 
         self.assertEqual(check_ingred('no ingredients', self.s_prog), None)
 
     def test_filter_naive(self):
-        filtered_ingreds = filter_naive(['apple', 'apple sauce', 'no ingredients'], self.filters)
+        filtered_ingreds = filter_naive(
+            ['apple', 'apple sauce', 'no ingredients'], self.filters)
         self.assertCountEqual(filtered_ingreds, ['apple sauce', 'apple'])
 
         filtered_ingreds = filter_naive(
@@ -33,6 +37,24 @@ class TestScrape(unittest.TestCase):
 
         filtered_ingreds = filter_naive([''], self.filters)
         self.assertCountEqual(filtered_ingreds, [])
+
+    def test_lemmatize(self):
+        self.assertEqual(lemmatize('onions'), 'onion')
+        self.assertEqual(lemmatize('apples'), 'apple')
+        self.assertEqual(lemmatize('tomatoes'), 'tomato')
+        self.assertEqual(lemmatize('anchovies'), 'anchovy')
+
+        self.assertEqual(lemmatize('chiles'), 'chile')
+        self.assertEqual(lemmatize('chilis'), 'chile')
+        self.assertEqual(lemmatize('chilies'), 'chile')
+        self.assertEqual(lemmatize('chillies'), 'chile')
+
+        self.assertEqual(lemmatize('white onions'), 'white onion')
+        self.assertEqual(lemmatize('green apples'), 'green apple')
+        self.assertEqual(lemmatize('red chillies'), 'red chile')
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
