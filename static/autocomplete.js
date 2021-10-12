da2655db6e8f1a083cf1a5f1bd69a746c3227c96
Josh Minch <josh.minch@gmail.com>
2021-10-11 18:56:25 -0700

    Implement levenshtein sorting for autocomplete

--- static/autocomplete.js
+++ static/autocomplete.js
@@ -1,7 +1,13 @@
-var all_ingreds = new Bloodhound({
+import { distance } from "./fastest-levenshtein.1.0.12.min.js";
+
+let all_ingreds = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.whitespace,
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: 'static/all_ingreds_filtered.json'
+    prefetch: 'static/all_ingreds_filtered.json',
+    sorter: function (a, b) {
+        let input_string = document.getElementById('search-input').value
+        return distance(a, input_string) - distance(b, input_string);
+    }
 });
 
 $('#bloodhound .typeahead').typeahead({
