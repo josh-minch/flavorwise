bcdee5bba9c8a7013c026eea7fe942263bbc7d25
Josh Minch <josh.minch@gmail.com>
2021-11-26 21:56:19 -0800

    Remove unnecessary on ready function, add perf log

--- static/script.js
+++ static/script.js
@@ -1,80 +1,79 @@
-$(document).ready(function () {
-    $.fn.DataTable.ext.pager.numbers_length = 9;
+$.fn.DataTable.ext.pager.numbers_length = 9;
 
-    const ingredTablePercent = 0.75;
-    const ingredTableUsableHeight = ingredTablePercent * window.innerHeight;
-    const numIngredTableRows = Math.round(ingredTableUsableHeight / 50);
-    const numRecipeTableRows = 4;
+const ingredTablePercent = 0.75;
+const ingredTableUsableHeight = ingredTablePercent * window.innerHeight;
+const numIngredTableRows = Math.round(ingredTableUsableHeight / 50);
+const numRecipeTableRows = 4;
 
 
-    $('#ingred-table').DataTable({
-        dom: "<if>t<lp>",
-        deferRender: true,
-        ajax: {
-            url: "/init_r_ingred_data",
-            dataSrc: "",
-        },
-        autoWidth: false,
-        columnDefs: [
-            { width: '19%', targets: 2 },
-            { width: '14%', targets: 3 },
-            { className: "pl-0", "targets": [0] },
-            { className: "text-right pr-0", "targets": [3] },
-            { orderable: false, targets: [0, 3] },
-            { orderSequence: ["desc", "asc"], targets: [1, 2] },
-            {
-                targets: -1,
-                render: function (data, type, row) {
-                    let btn = '<button type="button" class="r-ingred-btn btn btn-outline-primary" name="r_ingred" value="' + row[0] + '">Add</button>';
-                    return btn;
-                }
+$('#ingred-table').DataTable({
+    dom: "<if>t<lp>",
+    deferRender: true,
+    ajax: {
+        url: "/init_r_ingred_data",
+        dataSrc: "",
+    },
+    autoWidth: false,
+    columnDefs: [
+        { width: '19%', targets: 2 },
+        { width: '14%', targets: 3 },
+        { className: "pl-0", "targets": [0] },
+        { className: "text-right pr-0", "targets": [3] },
+        { orderable: false, targets: [0, 3] },
+        { orderSequence: ["desc", "asc"], targets: [1, 2] },
+        {
+            targets: -1,
+            render: function (data, type, row) {
+                let btn = '<button type="button" class="r-ingred-btn btn btn-outline-primary" name="r_ingred" value="' + row[0] + '">Add</button>';
+                return btn;
             }
-        ],
-        order: [[1, "desc"]],
-        pageLength: numIngredTableRows,
-        lengthChange: false,
-        language: {
-            emptyTable: " ",
-            info: "_TOTAL_ results",
-            infoEmpty: "0 results",
-            search: "_INPUT_",
-            searchPlaceholder: 'Filter results',
-            infoFiltered: "(filtered)",
-            zeroRecords: "No recommendations match your search filter. Bummer dude!",
-            paginate: {
-                previous: "Prev"
-            }
-        },
-        initComplete: function () {
-            hideTableIfEmpty($('#ingred-table'));
         }
-    });
+    ],
+    order: [[1, "desc"]],
+    pageLength: numIngredTableRows,
+    lengthChange: false,
+    language: {
+        emptyTable: " ",
+        info: "_TOTAL_ results",
+        infoEmpty: "0 results",
+        search: "_INPUT_",
+        searchPlaceholder: 'Filter results',
+        infoFiltered: "(filtered)",
+        zeroRecords: "No recommendations match your search filter. Bummer dude!",
+        paginate: {
+            previous: "Prev"
+        }
+    },
+    initComplete: function () {
+        hideTableIfEmpty($('#ingred-table'));
+    }
+});
 
-
-    $('#recipe-table').DataTable({
-        dom: "<if>t<lp>",
-        deferRender: true,
-        ajax: {
-            url: "/init_recipe_data",
-            dataSrc: "",
-            cache: true
+console.time("recipes")
+let recipesTable = $('#recipe-table').DataTable({
+    dom: "<if>t<lp>",
+    deferRender: true,
+    ajax: {
+        url: "/init_recipe_data",
+        dataSrc: "",
+        cache: true
+    },
+    autoWidth: false,
+    columns: [
+        { width: '100%' }
+    ],
+    columnDefs: [
+        {
+            visible: false,
+            targets: [1, 2],
+            searchable: false
         },
-        autoWidth: false,
-        columns: [
-            { width: '100%' }
-        ],
-        columnDefs: [
-            {
-                visible: false,
-                targets: [1, 2],
-                searchable: false
-            },
-            { className: "pl-0", "targets": [0] },
-            {
-                targets: 0,
-                render: function (data, type, row) {
-                    if (data) {
-                        const recipe_card = `
+        { className: "pl-0", "targets": [0] },
+        {
+            targets: 0,
+            render: function (data, type, row) {
+                if (data) {
+                    const recipe_card = `
                         <div class="media position-relative">
                             <img src="" data-src="${row[2]}" class="recipe-image">
                             <div class="media-body">
@@ -88,63 +87,62 @@ $(document).ready(function () {
                             </div>
                         </div>
                         `;
-                        return recipe_card;
-                    }
+                    return recipe_card;
                 }
             }
-        ],
-        pageLength: numRecipeTableRows,
-        lengthChange: false,
-        language: {
-            emptyTable: "Add ingredients to see recipes",
-            info: "_TOTAL_ results",
-            infoEmpty: "0 results",
-            infoFiltered: "(filtered)",
-            search: "_INPUT_",
-            searchPlaceholder: 'Filter results',
-            zeroRecords: "No recipes match your search filter. Bummer dude!",
-            paginate: {
-                previous: "Prev"
-            }
-        },
-        rowCallback: function (row, data) {
-            setRecipeHover()
-        },
-        drawCallback: function (settings) {
-            $("#recipe-table img:visible").unveil();
-            setRecipeHover()
-        },
-        initComplete: function () {
-            hideTableIfEmpty($('#recipe-table'));
         }
-    });
-
-    function setRecipeHover() {
-        $(".recipe-link").hover(
-            function () {
-                const hoverBorderColor = 'rgb(77, 77, 77)';
-                $(this).parent().siblings(".recipe-title").children().css('text-decoration', 'underline');
-                $(this).siblings(".link-icon").css('color', 'rgb(0, 86, 179)');
-                $(this).parent().parent().siblings(".recipe-image").css('border-color', hoverBorderColor);
-                $(this).parent().parent().parent().css('border-color', hoverBorderColor);
-                $(this).parent().parent().parent().toggleClass('drop-shadow');
-            }, function () {
-                const borderColor = '#dee2e6';
-                $(this).parent().siblings(".recipe-title").children().css('text-decoration', 'none');
-                $(this).siblings(".link-icon").css('color', '#007bff');
-                $(this).parent().parent().siblings(".recipe-image").css('border-color', borderColor);
-                $(this).parent().parent().parent().css('border-color', borderColor);
-                $(this).parent().parent().parent().toggleClass('drop-shadow');
-            }
-        );
+    ],
+    pageLength: numRecipeTableRows,
+    lengthChange: false,
+    language: {
+        emptyTable: "Add ingredients to see recipes",
+        info: "_TOTAL_ results",
+        infoEmpty: "0 results",
+        infoFiltered: "(filtered)",
+        search: "_INPUT_",
+        searchPlaceholder: 'Filter results',
+        zeroRecords: "No recipes match your search filter. Bummer dude!",
+        paginate: {
+            previous: "Prev"
+        }
+    },
+    rowCallback: function (row, data) {
+        setRecipeHover()
+    },
+    drawCallback: function (settings) {
+        $("#recipe-table img:visible").unveil();
+        setRecipeHover()
+    },
+    initComplete: function () {
+        hideTableIfEmpty($('#recipe-table'));
     }
+});
+console.timeEnd("recipes")
 
-    // Remove bootstrap class from datatable filters for easier custom styling
-    const tableFilters = document.querySelectorAll('.dataTables_filter label input');
-    tableFilters.forEach(filter => {
-        filter.classList.remove("form-control", "form-control-sm")
-    });
+function setRecipeHover() {
+    $(".recipe-link").hover(
+        function () {
+            const hoverBorderColor = 'rgb(77, 77, 77)';
+            $(this).parent().siblings(".recipe-title").children().css('text-decoration', 'underline');
+            $(this).siblings(".link-icon").css('color', 'rgb(0, 86, 179)');
+            $(this).parent().parent().siblings(".recipe-image").css('border-color', hoverBorderColor);
+            $(this).parent().parent().parent().css('border-color', hoverBorderColor);
+            $(this).parent().parent().parent().toggleClass('drop-shadow');
+        }, function () {
+            const borderColor = '#dee2e6';
+            $(this).parent().siblings(".recipe-title").children().css('text-decoration', 'none');
+            $(this).siblings(".link-icon").css('color', '#007bff');
+            $(this).parent().parent().siblings(".recipe-image").css('border-color', borderColor);
+            $(this).parent().parent().parent().css('border-color', borderColor);
+            $(this).parent().parent().parent().toggleClass('drop-shadow');
+        }
+    );
+}
 
+// Remove bootstrap class from datatable filters for easier custom styling
+const tableFilters = document.querySelectorAll('.dataTables_filter label input');
+tableFilters.forEach(filter => {
+    filter.classList.remove("form-control", "form-control-sm")
 });
 
 let checkboxes = document.querySelectorAll("input[type='checkbox']");
@@ -423,10 +421,9 @@ function appendRecipesTitleIngreds(titleSpan, recipes, curIngreds) {
 }
 
 function createRecipesBody(recipes) {
-    let table = $('#recipe-table').DataTable();
-
-    table.clear();
+    recipesTable.clear();
     var tableData = [];
+
     recipes.forEach(recipe => {
         const recipeName = recipe[0];
         const recipeLink = recipe[1];
@@ -435,6 +432,11 @@ function createRecipesBody(recipes) {
         const rowData = { '0': recipeName, '1': recipeLink, '2': recipeImage, '3': recipeSource };
         tableData.push(rowData);
     });
+    console.time("add");
+    recipesTable.rows.add(tableData);
+    console.timeEnd("add");
 
-    table.rows.add(tableData).draw();
+    console.time('draw');
+    recipesTable.draw(false);
+    console.timeEnd('draw');
 }
